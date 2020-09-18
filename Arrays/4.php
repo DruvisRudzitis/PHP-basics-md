6dc23f97bae3bc2ed3c78548c1c7f539616c065a
DruvisRudzitis <69005245+DruvisRudzitis@users.noreply.github.com>
2020-09-19 02:16:09 +0300

    Update 4.php

--- Arrays/4.php
+++ Arrays/4.php
@@ -1,27 +1,5 @@
 <?php
 
-//$a = rand(1, 100);
-//$b = rand(1, 100);
-
-
-//$array = range(1, 10);
-//$array= shuffle($array);
-
-//echo $array;
-
-//$array= [1, 2, 3, 4, 5];
-
-//echo $array[1];
-
-//$a = ['name'=>'Gaurav','age'=>23] ;
-//
-//$b = new ArrayObject($a) ;
-//
-//$b['test'] = true ;
-//
-//$c = $b->getArrayCopy() ;
-//
-//echo "<pre>",var_dump($c),"</pre>" ;
 
 $original = range(0, 100);
 shuffle($original);
